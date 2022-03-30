83bcd63cb21c5204b7de3de077d9e7c0b1b575b8
Julien Domagalski <julien.domagalski@gmail.com>
2022-03-30 05:33:42 +0400

    initial commit

---
+++ TaxiFareModel/params.py
@@ -0,0 +1,7 @@
+BUCKET_NAME="wagon-data-835-domagalski"
+BUCKET_TRAIN_DATA_PATH = "data/train_1k.csv"
+STORAGE_LOCATION = "models/TaxiFareModel/model.joblib"
+
+# Predict.py
+PATH_TO_LOCAL_MODEL = 'model.joblib'
+AWS_BUCKET_TEST_PATH = "s3://wagon-public-datasets/taxi-fare-test.csv"
